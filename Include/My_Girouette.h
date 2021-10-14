3563306eb5a409680d477b0e0f1c59d3a0726fe1
PierreLaur <pierre.laur31@gmail.com>
2021-10-14 12:15:31 +0200

    Progression Girouette

---
+++ Include/My_Girouette.h
@@ -0,0 +1,17 @@
+#ifndef MyGirouette
+#define MyGirouette
+#include "MyTimer.h"
+#include "My_GPIO.h"
+
+void gestion_girouette (void);//fonction gérant interruption toutes les 20ms
+
+void init_girouette (TIM_TypeDef*TimerEnc,TIM_TypeDef*TimerServo);//init les pin avant
+
+
+
+
+
+
+
+
+#endif
