c9585804aa643741fa908813981aee7b7d736d18
chamaben <chamabenlafkih.cb@gmail.com>
2021-11-15 09:40:42 +0100

    principal

--- principal.c
+++ principal.c
@@ -1,13 +1,26 @@
 #include "MyTimer.h"
 #include "My_Girouette.h"
 #include "My_GPIO.h"
+#include "UART.h"
 
 
 //pin A4, index girouette
 //pin A1, sortie pwm
 //pin A8 girouette A
 //pin A9 girouette B
-int main2 (void){
+int main (void){
+	
+	UART_config();
+	MyGPIO_Init (GPIOA, 6, Out_Ppull );
+	MyGPIO_Init (GPIOA, 9, AltOut_Ppull );
+	MyGPIO_Init (GPIOB, 6, AltOut_Ppull );
+	
+	
+	// timer3 Interruption 300ms
+	MyTimer_Base_Init (TIM3, 719, 30000);
+	MyTimer_Base_Start ( TIM3);
+	init_timer_PWM ();
+	MyTimer_ActiveIT  (TIM3 ,5, handling );
 	
 	init_girouette(TIM1,TIM2,GPIOA,4);
 	while(1){
